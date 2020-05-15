61659a814a5c9457c791a90e04b168c0e25f8b91
Victor Ochoa <ochoa.m.victor@gmail.com>
2020-05-15 11:08:01 -0700

    Geonames working

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,13 +1,9 @@
 function handleSubmit(event) {
    let cityName = document.getElementById('city-name').value;
-   console.log(cityName);
-   fetch('http://localhost:8081/trip',
-   {
-      method: "POST",
-      credentials: "same-origin",
-      headers: { "Content-Type": "application/json"},
-      body: JSON.stringify({ cityName })
-   })
+   let url = new URL('http://localhost:8081/trip?city=' + cityName);
+   // let params = {city: JSON.stringify({ cityName })};
+   // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+   fetch(url)
    .then(res => res.json())
    .then(function(res) {
    })
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
+const axios = require('axios');
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -17,16 +17,35 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.post('/trip', (req, res) => {
-    // const location = req.params.location
-    console.log("I'm in the body..")
-    console.log(res.body)
-    //
-    // geonamesAPI(location)
-    //     .then(coordenate => Promise.all([darkskyAPI(coordenate), pixabayAPI(location)]))
-    //     .then(([temperature, photo]) => ({temperature, photo}) )
-    //     .then( a => res.send(a))
-    //     .catch(error => {
-    //         console.log(error);
-    //     });
+app.get('/trip', (req, res) => {
+    let city = req.query.city;
+    getGeoNamesLocationData(city)
+    .then(response => getDarkSkyWeatherData(response))
+    .then(a => res.send(a))
+    .catch(error => {
+      console.log('an erorr at trip level')
+      console.error(error);
+    });
+    //   .then(locationInfo => getDarkSkyWeatherData(locationInfo))
 });
+
+function getGeoNamesLocationData(city) {
+  const baseURL = `http://api.geonames.org/postalCodeSearchJSON?placename=${city}`;
+  const auth = `&username=${process.env.GEO_NAMES_USERNAME}`;
+
+  return axios.get(baseURL + auth)
+    .then(response => response.data.postalCodes[0]);
+}
+
+let getDarkSkyWeatherData = (locationInfo) => {
+  console.log("in dark sky data")
+  console.log(locationInfo);
+  const darkSkyURL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/37.8267,-122.4233`
+  //DARK_SKY_API_KEY
+  // return axios.get(baseURL + auth)
+  //   .then(response => response.data.postalCodes[0]);
+}
+
+function errorOut() {
+
+}
