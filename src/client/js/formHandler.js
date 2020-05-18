e2eb8950891f1843a84e1456406337ac224a72e2
Victor Ochoa <ochoa.m.victor@gmail.com>
2020-05-17 21:52:57 -0700

    Integrated Weatherbit endpoint

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,12 +1,28 @@
+import { isDateValid, getTripDuration } from './dateUtils.js'
 function handleSubmit(event) {
    let cityName = document.getElementById('city-name').value;
-   let url = new URL('http://localhost:8081/trip?city=' + cityName);
+
    // let params = {city: JSON.stringify({ cityName })};
    // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-   fetch(url)
-   .then(res => res.json())
-   .then(function(res) {
-   })
+   let departDate = document.getElementById('depart-date').value;
+   let returnDate = document.getElementById('return-date').value;
+   if ( isDateValid(departDate) ) {
+     let withinWeek = false;
+     if (getTripDuration(departDate, returnDate) <= 7) {
+       withinWeek = true;
+     }
+     let departureDateObj = new Date(Date.parse(departDate));
+     let returnDateObj = new Date(Date.parse(returnDate));
+     let departureFormatted = (departureDateObj.getFullYear()-1) + "-" + (departureDateObj.getMonth()+1) + "-" + departureDateObj.getDate();
+     let oneDayLater = (departureDateObj.getFullYear()-1) + "-" + (departureDateObj.getMonth()+1) + "-" + (departureDateObj.getDate()+1);
+     let url = new URL('http://localhost:8081/trip?city=' + cityName + '&withinWeek=' + withinWeek + '&departure=' + departureFormatted + '&dayAfter=' + oneDayLater);
+     fetch(url)
+       .then(res => res.json())
+       .then(function(res) {
+       })
+   }
+
+
 
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -8,9 +8,6 @@ app.use(cors())
 app.use(express.json());
 app.use(express.static('dist'))
 
-
-
-
 console.log(__dirname)
 
 app.listen(8081, function () {
@@ -19,16 +16,18 @@ app.listen(8081, function () {
 
 app.get('/trip', (req, res) => {
     let city = req.query.city;
+    let getCurrentForecast = req.query.withinWeek == 'true';
+    let departureDate = req.query.departure;
+    let dayAfter = req.query.dayAfter;
     getGeoNamesLocationData(city)
-    .then(response => getDarkSkyWeatherData(response))
-    .then(a => res.send(a))
-    .catch(error => {
-      console.error(error);
-    });
-    //   .then(locationInfo => getDarkSkyWeatherData(locationInfo))
+      .then(response => getForecast(response, getCurrentForecast, departureDate, dayAfter))
+      .then(a => res.send(a))
+      .catch(error => {
+        console.error(error);
+      });
 });
 
-function getGeoNamesLocationData(city) {
+let getGeoNamesLocationData = (city) => {
   const baseURL = `http://api.geonames.org/postalCodeSearchJSON?placename=${city}`;
   const auth = `&username=${process.env.GEO_NAMES_USERNAME}`;
 
@@ -36,15 +35,19 @@ function getGeoNamesLocationData(city) {
     .then(response => response.data.postalCodes[0]);
 }
 
-let getDarkSkyWeatherData = (locationInfo) => {
-  console.log("in dark sky data")
-  console.log(locationInfo);
-  const darkSkyURL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${locationInfo.lat},${locationInfo.lng}`;
-  console.log(darkSkyURL)
-  // return axios.get(baseURL + auth)
-  //   .then(response => response.data.postalCodes[0]);
-}
+let getForecast = (locationInfo, getCurrentForecast, departureDate, dayAfter) => {
+  const baseURL = `http://api.weatherbit.io/v2.0/`;
+  let params = '';
 
-function errorOut() {
+  if (getCurrentForecast) {
+    params = `current?key=${process.env.WEATHERBIT_API_KEY}&lat=${locationInfo.lat}&lon=${locationInfo.lng}`;
+  } else {
+    params = `history/daily?key=${process.env.WEATHERBIT_API_KEY}&lat=${locationInfo.lat}&lon=${locationInfo.lng}&start_date=${departureDate}&end_date=${dayAfter}`;
+  }
 
+  return axios.get(baseURL + params)
+    .then(response => {
+      console.log(response.data)
+            console.log(response.data.data[0])
+    });
 }
